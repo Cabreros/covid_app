cdc25015659c3efd9b5b1e36c51db3d773d0daac
cabreros <cabreros17@gmail.com>
2021-04-09 11:22:07 -0400

    added assets and completed getprovincesummary

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -1,5 +1,4 @@
 import 'package:current_cases_app/services/data_service.dart';
-import 'package:current_cases_app/services/summary_model.dart';
 import 'package:current_cases_app/widgets/stats_card.dart';
 import 'package:flutter/material.dart';
 
@@ -9,18 +8,15 @@ class HomeScreen extends StatefulWidget {
 }
 
 class _HomeScreenState extends State<HomeScreen> {
-  final _dataService = DataService();
-  Summary _response;
   String _province = 'ON';
-  List<String> _provinces = ['ON', 'YK', 'AB', 'QC'];
+  List<String> _provinces = ['AB', 'BC', 'MB', 'NB', 'ON', 'QC', 'YT'];
+  DateTime selectedDate = DateTime.now();
 
   @override
   Widget build(BuildContext context) {
-    final screenHeight = MediaQuery.of(context).size.height;
-
     return Scaffold(
       body: CustomScrollView(
-        slivers: [_appBar(), _header(), _body(_province)],
+        slivers: [_appBar(), _header(), _bodyStats(_province), _datePicker()],
       ),
     );
   }
@@ -28,13 +24,20 @@ class _HomeScreenState extends State<HomeScreen> {
   SliverAppBar _appBar() {
     return SliverAppBar(
       backgroundColor: Color(0xffe27d60),
+      title: Text(
+        'Current Case Statistics',
+        style: TextStyle(
+          color: Colors.white,
+          fontSize: 23.0,
+          fontFamily: 'Futura',
+        ),
+      ),
       actions: [
         IconButton(
           icon: const Icon(Icons.notifications),
           onPressed: null,
         )
       ],
-      //title: Text('Floating app bar'),
       floating: true,
     );
   }
@@ -43,7 +46,6 @@ class _HomeScreenState extends State<HomeScreen> {
     return SliverToBoxAdapter(
       child: Container(
         padding: EdgeInsets.symmetric(horizontal: 15.0),
-        height: 100.0,
         decoration: BoxDecoration(
           color: Color(0xffe27d60),
           borderRadius: BorderRadius.only(
@@ -59,7 +61,7 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Expanded(
                   child: Text(
-                    'COVID-19',
+                    'Daily Summary',
                     style: TextStyle(
                       color: Colors.white,
                       fontSize: 27.0,
@@ -103,7 +105,6 @@ class _HomeScreenState extends State<HomeScreen> {
                       onChanged: (String value) {
                         setState(() {
                           _province = value;
-                          _makeRequest();
                         });
                       },
                     ),
@@ -114,21 +115,13 @@ class _HomeScreenState extends State<HomeScreen> {
             SizedBox(
               height: 10.0,
             ),
-            Text(
-              'Current Case Statistics',
-              style: TextStyle(
-                color: Colors.white,
-                fontSize: 23.0,
-                fontFamily: 'Futura',
-              ),
-            ),
           ],
         ),
       ),
     );
   }
 
-  SliverToBoxAdapter _body(province) {
+  SliverToBoxAdapter _bodyStats(province) {
     return SliverToBoxAdapter(
       child: Container(
         decoration: BoxDecoration(
@@ -139,18 +132,56 @@ class _HomeScreenState extends State<HomeScreen> {
           ),
         ),
         child: FutureBuilder(
-          future: DataService().getProvinceSummary(province), // async work
+          future: DataService()
+              .getProvinceSummary(province, DateTime.now()), // async work
           builder: (context, snapshot) {
             switch (snapshot.connectionState) {
               case ConnectionState.waiting:
-                return Text('Loading....');
+                return Center(
+                  child: CircularProgressIndicator(),
+                );
               default:
                 if (snapshot.hasError)
                   return Text('Error: ${snapshot.error}');
                 else {
                   var summary = snapshot.data;
                   return Column(
+                    crossAxisAlignment: CrossAxisAlignment.start,
                     children: [
+                      Container(
+                        padding: EdgeInsets.symmetric(
+                            horizontal: 10.0, vertical: 5.0),
+                        child: Text(
+                          'Updated as of: ' +
+                              summary.date.toString() +
+                              'selected date ' +
+                              selectedDate.toString(),
+                          style: TextStyle(
+                            color: Colors.black,
+                            fontSize: 10.0,
+                            fontFamily: 'Futura',
+                          ),
+                        ),
+                      ),
+                      Row(
+                        children: [
+                          StatsCard(
+                            label: 'Active',
+                            stat: summary.activeCases.toInt().toString(),
+                            color: Color(0xffc38d9e),
+                          ),
+                          StatsCard(
+                            label: 'Recovered',
+                            stat: summary.culRecovered.toInt().toString(),
+                            color: Color(0xff85dcba),
+                          ),
+                          StatsCard(
+                            label: 'Deaths',
+                            stat: summary.deaths.toInt().toString(),
+                            color: Color(0xffe27d60),
+                          ),
+                        ],
+                      ),
                       Row(
                         children: [
                           StatsCard(
@@ -160,25 +191,7 @@ class _HomeScreenState extends State<HomeScreen> {
                           StatsCard(
                             label: 'Total Deaths',
                             stat: summary.culDeaths.toInt().toString(),
-                            color: Color(0xffc38d9e),
-                          ),
-                        ],
-                      ),
-                      Row(
-                        children: [
-                          StatsCard(
-                            label: 'Active',
-                            stat: summary.activeCases.toInt().toString(),
-                          ),
-                          StatsCard(
-                            label: 'Recovered',
-                            stat: summary.culRecovered.toInt().toString(),
-                            color: Color(0xffc38d9e),
-                          ),
-                          StatsCard(
-                            label: 'Deaths',
-                            stat: summary.deaths.toInt().toString(),
-                            color: Color(0xffc38d9e),
+                            color: Color(0xffe8a87c),
                           ),
                         ],
                       ),
@@ -192,10 +205,27 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 
-  void _makeRequest() async {
-    final response = await _dataService.getProvinceSummary(_province);
-    setState(() {
-      _response = response;
-    });
+  SliverToBoxAdapter _datePicker() {
+    return SliverToBoxAdapter(
+      child: Container(
+        height: 25.0,
+        child: ElevatedButton(
+          onPressed: () => _selectDate(context),
+          child: Text('Select date'),
+        ),
+      ),
+    );
+  }
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime picked = await showDatePicker(
+        context: context,
+        initialDate: selectedDate,
+        firstDate: DateTime(2020, 3),
+        lastDate: DateTime.now());
+    if (picked != null && picked != selectedDate)
+      setState(() {
+        selectedDate = picked;
+      });
   }
 }
