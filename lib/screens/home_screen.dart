31446dd7145a25ee8552de0620d4751491148e2f
cabreros <cabreros17@gmail.com>
2021-06-25 11:31:38 -0400

    finished pie chart ui

--- lib/screens/home_screen.dart
+++ lib/screens/home_screen.dart
@@ -243,6 +243,9 @@ class _HomeScreenState extends State<HomeScreen> {
   }
 
   SliverToBoxAdapter _vaxCharts() {
+    MediaQueryData queryData = MediaQuery.of(context);
+    var formatter = NumberFormat('###,###,000');
+
     List<charts.Series<Vax, String>> _createSampleData() {
       final data = [
         new Vax(7576624, 'Single Dose'),
@@ -256,7 +259,7 @@ class _HomeScreenState extends State<HomeScreen> {
           domainFn: (Vax sales, _) => sales.label,
           measureFn: (Vax sales, _) => sales.pop,
           data: data,
-          labelAccessorFn: (Vax sales, _) => '${sales.pop}',
+          labelAccessorFn: (Vax sales, _) => '${formatter.format(sales.pop)}',
         )
       ];
     }
@@ -272,10 +275,15 @@ class _HomeScreenState extends State<HomeScreen> {
               children: [
                 Text(
                   'Current Vaccinations By The Numbers',
+                  style: TextStyle(
+                    color: Colors.black,
+                    fontSize: 12.0,
+                    fontFamily: 'Futura',
+                  ),
                 ),
                 SizedBox(
-                  height: 200,
-                  width: 200,
+                  height: 300,
+                  width: queryData.size.width - 50,
                   child: charts.PieChart(
                     _createSampleData(),
                     animate: false,
@@ -283,6 +291,36 @@ class _HomeScreenState extends State<HomeScreen> {
                       arcWidth: 60,
                       arcRendererDecorators: [new charts.ArcLabelDecorator()],
                     ),
+                    behaviors: [
+                      new charts.DatumLegend(
+                        // Positions for "start" and "end" will be left and right respectively
+                        // for widgets with a build context that has directionality ltr.
+                        // For rtl, "start" and "end" will be right and left respectively.
+                        // Since this example has directionality of ltr, the legend is
+                        // positioned on the right side of the chart.
+                        position: charts.BehaviorPosition.end,
+                        // For a legend that is positioned on the left or right of the chart,
+                        // setting the justification for [endDrawArea] is aligned to the
+                        // bottom of the chart draw area.
+                        outsideJustification:
+                            charts.OutsideJustification.endDrawArea,
+                        // By default, if the position of the chart is on the left or right of
+                        // the chart, [horizontalFirst] is set to false. This means that the
+                        // legend entries will grow as new rows first instead of a new column.
+                        horizontalFirst: false,
+                        // By setting this value to 2, the legend entries will grow up to two
+                        // rows before adding a new column.
+                        desiredMaxRows: 3,
+                        // This defines the padding around each legend entry.
+                        cellPadding:
+                            new EdgeInsets.only(right: 4.0, bottom: 4.0),
+                        // Render the legend entry text with custom styles.
+                        entryTextStyle: charts.TextStyleSpec(
+                            color: charts.MaterialPalette.black,
+                            fontFamily: 'Futura',
+                            fontSize: 12),
+                      )
+                    ],
                   ),
                 ),
               ],
